02eacf3b299a92481973ab3aa7d0bdc971350b69
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-13 20:35:10 +0400

    five task

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        int rub = (a * c) + b * c / 100;
-        int kop = b * c % 100;
-        System.out.println(rub + " " + kop);
+        int a = in.nextInt(),s,m,h;
+        if (a < 86400) {
+            s = a % 60;
+            m = (a / 60) % 60;
+            h = ((a / 60) / 60) % 60;
+        }
+        else {
+            s = (a % 86400) % 60;
+            m = ((a % 86400) / 60) % 60;
+            h = (((a % 86400) / 60) / 60) % 60;
+        }
+        String s1 = "", m1= "";
+        s1 = "" + s;
+        m1 = "" + m;
+        if (s < 10) {
+            s1 = "0" + s1;
+        }
+        if (m < 10) {
+            m1 = "0" + m1;
+        }
+        System.out.println(h + ":" + m1 + ":" + s1);
     }
 }

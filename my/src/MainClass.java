55e4188c1d14a89114b1364cc59952cdad975313
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-13 20:32:36 +0400

    first task

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.println(a + b);
+        int N = in.nextInt();
+        System.out.println(N % 10);
     }
 }

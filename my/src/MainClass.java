816425248dc68ce988687642bd61e7bd86556f07
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-13 20:34:37 +0400

    fourth task

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        System.out.println((N + 2) - N % 2);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int rub = (a * c) + b * c / 100;
+        int kop = b * c % 100;
+        System.out.println(rub + " " + kop);
     }
 }

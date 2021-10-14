efd2f0dd5eabf6e182b0d29bea39cb657ac2d4cb
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-14 23:23:51 +0400

    boolean first

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -3,26 +3,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt(),s,m,h;
-        if (a < 86400) {
-            s = a % 60;
-            m = (a / 60) % 60;
-            h = ((a / 60) / 60) % 60;
-        }
-        else {
-            s = (a % 86400) % 60;
-            m = ((a % 86400) / 60) % 60;
-            h = (((a % 86400) / 60) / 60) % 60;
-        }
-        String s1 = "", m1= "";
-        s1 = "" + s;
-        m1 = "" + m;
-        if (s < 10) {
-            s1 = "0" + s1;
-        }
-        if (m < 10) {
-            m1 = "0" + m1;
-        }
-        System.out.println(h + ":" + m1 + ":" + s1);
+        int a = in.nextInt();
+        System.out.println(a >= 3 && a <= 8);
     }
 }

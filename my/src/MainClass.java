f5eba53799a85e9f858688b41b895d01daef51fe
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-14 23:33:08 +0400

    boolean third

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -4,6 +4,6 @@ public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         float a = in.nextFloat();
-        System.out.println((a >= -3 && a <= 5) || (a >= 9 && a <= 15));
+        System.out.println(!((a >= -2 && a <= 3) || (a >= 6 && a <= 9)));
     }
 }

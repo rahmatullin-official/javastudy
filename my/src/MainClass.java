e171c45d09fe01e3c1b2129df47e6cb6faceb547
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-14 23:52:56 +0400

    boolean six

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt(), d = in.nextInt();
-        System.out.println((-a == b || -a == c || -a == d) || (-b == a || -b == c || -b == d) || (-c == a || -c == b || -c == d) || (-d == a || -d == b || -d == c));
+        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
+        System.out.println((a % 2 == 0 && b % 2 == 0) || (a % 2 == 0 && c % 2 == 0) || (b % 2 == 0 && c % 2 == 0));
     }
 }

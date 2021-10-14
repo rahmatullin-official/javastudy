1f6e9d61058616933082bca35d95703c204dec45
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-14 23:27:57 +0400

    boolean second

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        System.out.println(a >= 3 && a <= 8);
+        float a = in.nextFloat();
+        System.out.println((a >= -3 && a <= 5) || (a >= 9 && a <= 15));
     }
 }

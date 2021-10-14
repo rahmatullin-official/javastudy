9aa4d3bc917032e37dbef593367a613570fa8cd2
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-14 23:50:05 +0400

    bollean five

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        System.out.println(a % 5 == 0 && (a > 99 && a < 1000));
+        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt(), d = in.nextInt();
+        System.out.println((-a == b || -a == c || -a == d) || (-b == a || -b == c || -b == d) || (-c == a || -c == b || -c == d) || (-d == a || -d == b || -d == c));
     }
 }

f7059efffad8bbc2e47124f780d127471d9a9293
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-14 23:37:33 +0400

    boolean fourth

--- my/src/MainClass.java
+++ my/src/MainClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        float a = in.nextFloat();
-        System.out.println(!((a >= -2 && a <= 3) || (a >= 6 && a <= 9)));
+        int a = in.nextInt();
+        System.out.println(a % 5 == 0 && (a > 99 && a < 1000));
     }
 }
